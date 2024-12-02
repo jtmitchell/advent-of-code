48f596a8064f793328643ea8525ddf9ec1c1cfd0
James Mitchell <james.mitchell@maungawhau.net.nz>
2024-12-03 11:32:38 +1300

    Start day 2

---
+++ advent_of_code/day_02/README.md
@@ -0,0 +1,12 @@
+# [Day 2: Red-Nosed Reports](https://adventofcode.com/2024/day/2)
+
+## Puzzle 1
+Determine which reports are safe.
+
+For each line in the input data, the report is safe if:
+* The levels are either all increasing or all decreasing.
+* Any two adjacent levels differ by at least one and at most three.
+
+
+## Puzzle 2
+TODO
---
+++ advent_of_code/day_02/puzzle.py
@@ -0,0 +1,46 @@
+"""
+Advent of Code Puzzle.
+Day 2: Red-Nosed Reports
+"""
+
+import pathlib
+from argparse import Namespace
+
+
+def run_puzzle(args: Namespace):
+    """
+    Run the puzzle.
+    """
+
+    input_file = pathlib.Path(__file__).with_name("input.txt")
+    data = load_data(input_file)
+
+    # Print the solution
+    result = solve_pt1(data) if not args.part2 else solve_pt2(data)
+    print(f"Result is {result}")
+
+
+def load_data(datafile: str) -> list[list[int]]:
+    """
+    Load the puzzle data.
+    """
+    print(f"loading {datafile}")
+    data = []
+    with open(datafile, encoding="utf8") as fh:
+        for line in fh:
+            data.append(line.split())
+    return data
+
+def solve_pt1(data: list[list[int]]):
+    """
+    Solve the part one puzzle.
+    """
+    for report in data:
+        pass
+
+
+def solve_pt2(data:list[list[int]]):
+    """
+    Solve the part two puzzle.
+    """
+    return None
