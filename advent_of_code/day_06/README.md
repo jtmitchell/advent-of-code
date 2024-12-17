9ee7894339b68ada558c0311182505fc7a257995
James Mitchell <james.mitchell@maungawhau.net.nz>
2024-12-17 18:37:29 +1300

    WIP solve for day6 part 1

--- advent_of_code/day_06/README.md
+++ advent_of_code/day_06/README.md
@@ -1,7 +1,7 @@
 # [Day 6: Guard Gallivant](https://adventofcode.com/2024/day/6)
 
 ## Puzzle 1
-TODO
+Plot the guards movements until they leave the room. Count the number of unique locations visited.
 
 ## Puzzle 2
 TODO
--- advent_of_code/day_06/puzzle.py
+++ advent_of_code/day_06/puzzle.py
@@ -5,6 +5,39 @@ Day 6: Guard Gallivant
 
 import pathlib
 from argparse import Namespace
+from dataclasses import dataclass, field
+
+from utils.vector import Vector
+
+
+@dataclass
+class Room:
+    height: int = 0
+    width: int = 0
+    obstructions: list[Vector] = field(default_factory=list)
+    guard_location: Vector = field(default_factory=Vector)
+    guard_direction: Vector = field(default_factory=Vector)
+
+    @property
+    def is_guard_in_room(self):
+        return all(
+            [
+                0 <= self.guard_location.x < self.width,
+                0 <= self.guard_location.y < self.height,
+            ]
+        )
+
+    def move(self, distance: int = 1) -> list[Vector]:
+        steps = []
+        new_location = self.guard_location + (distance * self.guard_direction)
+        if new_location in self.obstructions:
+            # Rotate 90 degrees
+            self.guard_direction = Vector(x=-self.guard_direction.y, y=self.guard_direction.x)
+        else:
+            self.guard_location = new_location
+            steps.append(new_location)
+
+        return steps
 
 
 def run_puzzle(args: Namespace):
@@ -20,24 +53,53 @@ def run_puzzle(args: Namespace):
     print(f"Result is {result}")
 
 
-def load_data(datafile: str):
+def load_data(datafile: str) -> Room:
     """
     Load the puzzle data.
     """
     print(f"loading {datafile}")
+    room = Room()
     with open(datafile, encoding="utf8") as fh:
-        for line in fh:
-            line.split()
+        # Because I'm using enumerate for each line and character
+        # my coordinate origin (0,0) is the TOP LEFT corner
+        # with Y increasing DOWN the page
+        for row, line in enumerate(fh):
+            room.height = row
+            for column, char in enumerate(line):
+                room.width = column
+                location = Vector(x=column, y=row)
+                match char:
+                    case "#":
+                        room.obstructions.append(location)
+                    case "^":
+                        room.guard_location = location
+                        room.guard_direction = Vector(x=0, y=-1)
+                    case "<":
+                        room.guard_location = location
+                        room.guard_direction = Vector(x=-1, y=0)
+                    case ">":
+                        room.guard_location = location
+                        room.guard_direction = Vector(x=1, y=0)
+                    # Not sure what a downward pointing char is...
+                    case _:
+                        pass
+
+    return room
 
 
-def solve_pt1(data):
+def solve_pt1(data: Room):
     """
     Solve the part one puzzle.
     """
-    return None
+    visited = []
+    while data.is_guard_in_room:
+        visited.extend(data.move(distance=1))
+
+    distinct_locations = set(visited)
+    return len(distinct_locations)
 
 
-def solve_pt2(data):
+def solve_pt2(data: Room):
     """
     Solve the part two puzzle.
     """
