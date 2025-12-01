d1d934779657e571653791b84adaf4398fb225a5
James Mitchell <james.mitchell@maungawhau.net.nz>
2025-12-01 20:17:02 +0000

    Update template with type hints

--- advent_of_code/day_01/puzzle.py
+++ advent_of_code/day_01/puzzle.py
@@ -5,40 +5,42 @@ Day 1: Secret Entrance
 
 import pathlib
 from argparse import Namespace
+from pathlib import Path
+from typing import Any
 
 
-def run_puzzle(args: Namespace):
+def run_puzzle(args: Namespace) -> None:
     """
     Run the puzzle.
     """
 
-    input_file = pathlib.Path(__file__).with_name("input.txt")
-    data = load_data(input_file)
+    input_file: Path = pathlib.Path(__file__).with_name(name="input.txt")
+    data: Any = load_data(datafile=input_file)
 
     # Print the solution
-    result = solve_pt1(data) if not args.part2 else solve_pt2(data)
+    result: bool = solve_pt1(data) if not args.part2 else solve_pt2(data)
     print(f"Result is {result}")
 
 
-def load_data(datafile: str):
+def load_data(datafile: str) -> Any:
     """
     Load the puzzle data.
     """
     print(f"loading {datafile}")
-    with open(datafile, encoding="utf8") as fh:
+    with open(file=datafile, encoding="utf8") as fh:
         for line in fh:
             line.split()
 
 
-def solve_pt1(data):
+def solve_pt1(data) -> bool:
     """
     Solve the part one puzzle.
     """
-    return None
+    return False
 
 
-def solve_pt2(data):
+def solve_pt2(data) -> bool:
     """
     Solve the part two puzzle.
     """
-    return None
+    return False
--- templates/{{cookiecutter.module_name}}/test_puzzle.py
+++ templates/{{cookiecutter.module_name}}/test_puzzle.py
@@ -3,29 +3,30 @@ Unit test for Day {{cookiecutter.day_number}}: {{cookiecutter.puzzle_name}}
 """
 
 import pathlib
+from pathlib import Path
 
 from .puzzle import load_data, solve_pt1, solve_pt2
 
 
-def test_puzzle_pt1():
+def test_puzzle_pt1() -> None:
     """
     Run a test against the sample input.
     """
     expected_result = {{cookiecutter.expected_test_result}}
-    input_file = pathlib.Path(__file__).with_name("sample.txt")
+    input_file: Path = pathlib.Path(__file__).with_name(name="sample.txt")
 
-    data = load_data(input_file)
-    result = solve_pt1(data)
+    data = load_data(datafile=input_file)
+    result: bool = solve_pt1(data)
     assert result == expected_result
 
 
-def test_puzzle_pt2():
+def test_puzzle_pt2() -> None:
     """
     Run a test against the sample input.
     """
     expected_result = None
-    input_file = pathlib.Path(__file__).with_name("sample.txt")
+    input_file: Path = pathlib.Path(__file__).with_name(name="sample.txt")
 
-    data = load_data(input_file)
-    result = solve_pt2(data)
+    data = load_data(datafile=input_file)
+    result: bool = solve_pt2(data)
     assert result == expected_result
