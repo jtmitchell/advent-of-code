a99d5cd0582a2073265159ced1020f61fbad474c
James Mitchell <james.mitchell@maungawhau.net.nz>
2025-12-11 21:46:21 +1300

    Refactor Day 04

--- advent_of_code/day_04/models.py
+++ advent_of_code/day_04/models.py
@@ -26,6 +26,24 @@ class RoomLocation:
 @dataclass
 class Room:
     locations: list[RoomLocation] = field(default_factory=list)
+    _map: dict | None = None
+    max_x: int = 0
+    max_y: int = 0
+
+    def scan(self) -> None:
+        self._map = {}
+        for i in self.locations:
+            x_pos = i.location.x
+            y_pos = i.location.y
+            if x_pos not in self._map:
+                self._map[x_pos] = {}
+
+            if x_pos > self.max_x:
+                self.max_x = x_pos
+            if y_pos > self.max_y:
+                self.max_y = y_pos
+
+            self._map[x_pos][y_pos] = i.content
 
     def is_accessible(
         self,
@@ -34,13 +52,25 @@ class Room:
         limit: int = 4,
     ) -> bool:
         """Return True if the location has fewer than 4 paper rolls surrounding it."""
-        neighbour_locations = self.neighbours(location=location)
-        accessible_locations = [
+        if self._map is None:
+            self.scan()
+
+        possible_neighbour_locations = [
             i
-            for i in self.locations
-            if i.content == content.value and i.location in neighbour_locations
+            for i in self.neighbours(location=location)
+            if all(
+                [
+                    0 <= i.x <= self.max_x,
+                    0 <= i.y <= self.max_y,
+                ]
+            )
         ]
-        return len(accessible_locations) < limit
+        neighbours_with_paper = [
+            i
+            for i in possible_neighbour_locations
+            if self._map[i.x][i.y] == Content.PAPER.value
+        ]
+        return len(neighbours_with_paper) < limit
 
     def neighbours(self, location: RoomLocation) -> list[RoomLocation | None]:
         """Return a list of the neighbouring locations."""
