44aee78351591d76c6133418bba0486464b83132
James Mitchell <james.mitchell@maungawhau.net.nz>
2025-12-31 22:03:46 +1300

    WIP

--- advent_of_code/day_07/models.py
+++ advent_of_code/day_07/models.py
@@ -41,31 +41,65 @@ class Room:
 
     def activate_beam(self) -> int:
         self.beams = self.emit_beam(start_pt=self.start_pt)
-        return len(self.beams)
+        return self.unique_beams()
 
-    def emit_beam(self, start_pt: Vector) -> set[BeamPath]:
-        delta_y = Vector(x=0, y=-1)
-        next_pt = start_pt + delta_y
-        new_beams = set(BeamPath(start=start_pt))
+    def unique_beams(self) -> int:
+        """
+        Discard any overlapping beams.
+        """
+        # Sort the beams into columns
+        beam_cols = {}
+        for i in self.beams:
+            key = i.start.x
+            if key not in beam_cols:
+                beam_cols[key] = []
+            beam_cols[key].append(i)
 
-        if next_pt.y < 0:
-            # Reached the bottom
-            return new_beams
-
-        if next_pt in self.splitters:
-            # Split the beam and check the new start points
-            for delta_x in [Vector(x=-1, y=0), Vector(x=1, y=0)]:
-                new_beam_start = next_pt + delta_x
-                if any(
-                    [
-                        new_beam_start.x < 0,
-                        new_beam_start >= self.max_x,
-                        new_beam_start in self.splitters,
-                    ]
-                ):
-                    # Ignore anything that is outside the room
+        # Find overlapping beams
+        unique_beams = []
+        for i in self.beams:
+            key = i.start.x
+            found = False
+            for j in sorted(beam_cols[key], key=lambda x: (x.start.y, x.end.y)):
+                if i.start == j.start and i.end == j.end:
+                    # Skip the entry for this beam
                     continue
+                if j.start.y <= i.start.y and i.end.y <= j.end.y:
+                    # Overlapping beams
+                    found = True
+                    break
+            if not found:
+                unique_beams.append(i)
 
-                new_beams.union(self.emit_beam(start_pt=new_beam_start))
+        return len(unique_beams)
 
-        return new_beams
+    def emit_beam(self, start_pt: Vector) -> list[BeamPath]:
+        delta_y = Vector(x=0, y=1)
+        beam = BeamPath(start=start_pt, end=start_pt)
+        new_beams = [beam]
+        do_next_step = True
+
+        while do_next_step:
+            beam.end += delta_y
+            if beam.end.y >= self.max_y:
+                # Reached the bottom
+                do_next_step = False
+
+            elif beam.end in self.splitters:
+                # Split the beam and check the new start points
+                for delta_x in [Vector(x=-1, y=0), Vector(x=1, y=0)]:
+                    new_beam_start = beam.end + delta_x
+                    if any(
+                        [
+                            new_beam_start.x < 0,
+                            new_beam_start.x >= self.max_x,
+                            new_beam_start in self.splitters,
+                        ]
+                    ):
+                        # Ignore anything that is outside the room
+                        continue
+
+                    new_beams.extend(self.emit_beam(start_pt=new_beam_start))
+                do_next_step = False
+
+        return list(set(new_beams))
--- advent_of_code/day_07/test_puzzle.py
+++ advent_of_code/day_07/test_puzzle.py
@@ -12,18 +12,24 @@ from .puzzle import load_data, solve_pt1, solve_pt2
 
 
 @pytest.mark.parametrize(
-    "expected_result, filename",  # noqa: PT006
+    "expected_result, filename, num_lines",  # noqa: PT006
     [
-        (21, "sample.txt"),
+        (1, "sample.txt", 2),
+        (3, "sample.txt", 3),
+        (3, "sample.txt", 4),
+        (21, "sample.txt", None),
     ],
 )
-def test_puzzle_pt1(expected_result, filename) -> None:
+def test_puzzle_pt1(expected_result, filename, num_lines) -> None:
     """
     Run a test against the sample input.
     """
     input_file: Path = pathlib.Path(__file__).with_name(name=filename)
 
     data = load_data(datafile=input_file)
+
+    if num_lines is not None:
+        data = list(data)[:num_lines]
 
     context = pytest.raises(NotImplementedError) if expected_result is None else nullcontext()
     with context:
